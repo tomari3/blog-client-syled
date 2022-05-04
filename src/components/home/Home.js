8adb8317125ac0db72b12fd35162ca2abc1e2394
Tom Ari <freak.tom3@gmail.com>
2022-05-04 13:11:14 +0300

    basic fetch

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,19 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import { StyledHome } from "../../styles/StyledHome";
 
+const BaseUrl = "http://localhost:3000/";
+
 export const Home = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get(BaseUrl).then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
   return (
     <StyledHome>
       <h1>home</h1>
