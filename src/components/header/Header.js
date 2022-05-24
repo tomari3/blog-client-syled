b9611ca8224f271b005cdabae73ac7bfc3941f9f
Tom Ari <freak.tom3@gmail.com>
2022-05-24 13:07:32 +0300

    basic logout

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,28 @@
 import { Link } from "react-router-dom";
 
 import { useAuth } from "../../hooks/useAuth";
+import { axiosPrivate } from "../../utils/axios";
 
 import { StyledHeader } from "../../styles/StyledHeader";
+import { StyledButton } from "../../styles/StyledButton";
 
 export const Header = () => {
   const { auth } = useAuth();
+  const logout = async () => {
+    const payload = {
+      id: auth?._id,
+    };
+    const postUrl = `auth/logout`;
 
+    try {
+      const result = await axiosPrivate.get(postUrl, payload);
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+
+      setTimeout(() => {}, 5000);
+    }
+  };
   return (
     <StyledHeader>
       <Link to="/">home</Link>
@@ -18,9 +34,9 @@ export const Header = () => {
         </div>
       ) : (
         <div>
-          <Link to="/logout">logout</Link>
-          <br />
           <Link to="/profile">profile</Link>
+          <br />
+          <StyledButton onClick={logout}>logout</StyledButton>
         </div>
       )}
     </StyledHeader>
